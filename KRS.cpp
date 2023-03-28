1b1f03f03ef87b2894a68a8caa04f41a20378fe6
febrytriharkasa <129181564+febrytriharkasa@users.noreply.github.com>
2023-03-28 20:02:33 +0700

    Update KRS.cpp

--- KRS.cpp
+++ KRS.cpp
@@ -12,33 +12,51 @@ struct dosen{
 	int nidn;
 }dsn[10];
 
-int i;
+int i, k, j = 0, awal = 0, l;
 
-void mhsData(){
-	mhs[0].nama = "Kevin Yauwry";
-	mhs[0].ipk = "4.00";
-	mhs[0].prodi = "Teknik Informatika";
-	mhs[0].nim = 1462200164;
+void mhsData(int k){
+	for(i = 1; i < k;i++){
+		cout<<mhs[i].nama<<endl;
+		cout<<mhs[i].nim<<endl;
+		cout<<mhs[i].prodi<<endl;
+		cout<<mhs[i].ipk<<endl;
+	}
+	getch();
 }
 
-void mhsCari(){
+void mhsCari(int k){
 	int cariNim;
+	int ketemu = 0;
 	system("cls");
 	cout<<"CARI DATA MAHASISWA"<<endl;
 	cout<<"Masukkan Nim : ";
-	cin>>CariNim;
-	for(int i = 0;i<10;i++){
-		if(cariNim==mhs[i].nim){			
+	cin>>cariNim;
+	for(int i = 1; i < k;i++){
+		if(cariNim == mhs[i].nim){
+			ketemu = 1;
 			cout<<mhs[i].nama<<endl;
 			cout<<mhs[i].nim<<endl;
 			cout<<mhs[i].prodi<<endl;
-			cout<<mhs[i].prodi<<endl;
+			cout<<mhs[i].ipk<<endl;
 		}
 	}
+	if(ketemu == 0){
+		cout<<"Data Mahasiswa tidak ditemukan"<<endl;
+	}
+	getch();
 }
 
 int main(){
 	int pil;
+	if (awal == 0){
+		j =1;
+		mhs[j].nama  = "Kevin Yauwry";
+		mhs[j].ipk   = "4.00";
+		mhs[j].prodi = "Teknik Informatika";
+		mhs[j].nim   = 164;
+		awal++;
+		i=2;
+	}
 	while(pil!=5){
 		cout<<"MENU KRS"<<endl;
 		cout<<"1. Lihat Data Mahasiswa"<<endl;
@@ -48,8 +66,8 @@ int main(){
 		cout<<"5. Keluar"<<endl;
 		cout<<"Pilih : ";cin>>pil;
 		if(pil==1)
-		mhsData();
+		mhsData(i);
 		if(pil==2)
-		mhsCari();
+		mhsCari(i);
 	}
 }
