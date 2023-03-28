e69dd9fca4cd341a8e6290a34a0919ad4661032e
KevinYauwry <129181446+KevinYauwry@users.noreply.github.com>
2023-03-28 19:33:46 +0700

    Add files via upload

---
+++ KRS.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+struct mahasiswa{
+	string nama, prodi, ipk;
+	int nim;
+}mhs[10];
+
+struct dosen{
+	string nama;
+	int nidn;
+}dsn[10];
+
+void mhsData(){
+mhs[0].nama = "Kevin Yauwry";
+mhs[0].ipk = "4.00";
+mhs[0].prodi = "Teknik Informatika";
+mhs[0].nim = 1462200164;
+}
+
+void mhsCari(){
+	system("cls");
+	cout<<"CARI DATA MAHASISWA"<<endl;
+	for(int i = 0;i<10;i++){
+		cout<<mhs[i].nama;
+		cout<<mhs[i].nim;
+		cout<<mhs[i].prodi;
+		cout<<mhs[i].prodi;
+	}
+}
+
+int main(){
+	int pil;
+	while(pil!=4){
+		cout<<"MENU KRS"<<endl;
+		cout<<"1. Lihat Data Mahasiswa"<<endl;
+		cout<<"2. Cari Mahasiswa"<<endl;
+		cout<<"3. Lihat Data Dosen";
+		cout<<"4. Cari Dosen"<<endl;
+		cout<<"Pilih : ";cin>>pil;
+		if(pil==1)
+		mhsData();
+		if(pil==2)
+		mhsCari();
+	}
+}
