541a89eefb12a83c403fdeb2d3b3501c414cb2c4
faisalbustomi <129181948+faisalbustomi@users.noreply.github.com>
2023-03-28 20:48:53 +0700

    Update KRS.cpp

--- KRS.cpp
+++ KRS.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<conio.h>
+#include<windows.h>
 using namespace std;
 
 struct mahasiswa{
@@ -12,51 +13,75 @@ struct dosen{
 	int nidn;
 }dsn[10];
 
-int i, k, j = 0, awal = 0, l;
+COORD coord = {0, 0};
+void gotoxy(int x, int y)
+{
+    coord.X = x;
+    coord.Y = y;
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
+}
 
-void mhsData(int k){
-	for(i = 1; i < k;i++){
-		cout<<mhs[i].nama<<endl;
-		cout<<mhs[i].nim<<endl;
-		cout<<mhs[i].prodi<<endl;
-		cout<<mhs[i].ipk<<endl;
+int i;
+
+void mhsData(){
+	for(i = 1; i < 3;i++){
+		cout<<"Nama  : "<<mhs[i].nama<<endl;
+		cout<<"Nim   : "<<mhs[i].nim<<endl;
+		cout<<"Prodi : "<<mhs[i].prodi<<endl;
+		cout<<"Ipk   : "<<mhs[i].ipk<<endl;
+		cout<<"-------------------\n";
 	}
 	getch();
 }
 
-void mhsCari(int k){
-	int cariNim;
-	int ketemu = 0;
+void mhsCari(){
+	int cariNim, ketemu = 0;
 	system("cls");
 	cout<<"CARI DATA MAHASISWA"<<endl;
 	cout<<"Masukkan Nim : ";
 	cin>>cariNim;
-	for(int i = 1; i < k;i++){
-		if(cariNim == mhs[i].nim){
-			ketemu = 1;
-			cout<<mhs[i].nama<<endl;
-			cout<<mhs[i].nim<<endl;
-			cout<<mhs[i].prodi<<endl;
-			cout<<mhs[i].ipk<<endl;
+	for(int i = 1; i < 3;i++){
+		if(cariNim == mhs[i].nim){	
+			ketemu =1;		
+			cout<<"Nama  : "<<mhs[i].nama<<endl;
+			cout<<"Nim   : "<<mhs[i].nim<<endl;
+			cout<<"Prodi : "<<mhs[i].prodi<<endl;
+			cout<<"Ipk   : "<<mhs[i].ipk<<endl;
+			cout<<"|------------------------------------------------------------------------------|\n";	
+			cout<<"|                                      Krs                                     |\n";
+			cout<<"|------------------------------------------------------------------------------|\n";
+			cout<<"|No|         Matkul       |     Sks      |      Hari     |         Jam         |\n";
+			gotoxy(1,10);cout<<"1\n";
+			gotoxy(6,10);cout<<"Graf dan Automata\n";
+			gotoxy(33,10);cout<<"3\n";
+			gotoxy(47,10);cout<<"Senin\n";
+			gotoxy(62,10);cout<<"12.00 - 14.30\n";
+			gotoxy(0,10);cout<<"|\n";
+			gotoxy(3,10);cout<<"|\n";
+			gotoxy(26,10);cout<<"|\n";
+			gotoxy(41,10);cout<<"|\n";
+			gotoxy(57,10);cout<<"|\n";
+			gotoxy(79,10);cout<<"|\n";
 		}
 	}
 	if(ketemu == 0){
-		cout<<"Data Mahasiswa tidak ditemukan"<<endl;
+		cout<<"Nim Mahasiswa tidak ditemukan\n";
 	}
 	getch();
 }
 
 int main(){
 	int pil;
-	if (awal == 0){
-		j =1;
-		mhs[j].nama  = "Kevin Yauwry";
-		mhs[j].ipk   = "4.00";
-		mhs[j].prodi = "Teknik Informatika";
-		mhs[j].nim   = 164;
-		awal++;
-		i=2;
-	}
+	
+	mhs[1].nama  = "Kevin Yauwry";
+	mhs[1].ipk   = "4.00";
+	mhs[1].prodi = "Teknik Informatika";
+	mhs[1].nim   = 164;
+	mhs[2].nama  = " M. Faisal Bustomi";
+	mhs[2].ipk   = "-4.00";
+	mhs[2].prodi = "Teknik Informatika";
+	mhs[2].nim   = 157;
+		
 	while(pil!=5){
 		cout<<"MENU KRS"<<endl;
 		cout<<"1. Lihat Data Mahasiswa"<<endl;
@@ -66,8 +91,8 @@ int main(){
 		cout<<"5. Keluar"<<endl;
 		cout<<"Pilih : ";cin>>pil;
 		if(pil==1)
-		mhsData(i);
+		mhsData();
 		if(pil==2)
-		mhsCari(i);
+		mhsCari();
 	}
 }
